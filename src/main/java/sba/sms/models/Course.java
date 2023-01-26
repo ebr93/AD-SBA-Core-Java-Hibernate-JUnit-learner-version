44057b859ddc712b3f0a5d77e1a7255b37c63c0b
Edward Barcenas <edward.barcenas16@gmail.com>
2023-01-25 21:43:57 -0600

    completed models of Couse and Student

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,64 @@
 package sba.sms.models;
 
-public class Course {
+import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "course")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	@NonNull
+	@Column(length = 50)
+	String name;
+	@NonNull
+	@Column(length = 50)
+	String instructor;
+	@ToString.Exclude
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "courses")
+	List<Student> students;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(instructor, name, students);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		return Objects.equals(instructor, other.instructor) && Objects.equals(name, other.name)
+				&& Objects.equals(students, other.students);
+	}
+	
+	
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,61 @@
 package sba.sms.models;
 
-public class Student {
+import java.util.List;
+import java.util.jar.Attributes.Name;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "student")
+public class Student {
+	@Id
+	@Column(name = "email", length = 50)
+	String email;
+	@NonNull
+	@Column(name = "name", length = 50)
+	String name;
+	@NonNull
+	@Column(name = "password", length = 50)
+	String password;
+	@ToString.Exclude
+	@JoinTable(name = "student_courses",
+			joinColumns = @JoinColumn(
+					name = "student_email",
+					referencedColumnName = "email"
+			), 
+			inverseJoinColumns = @JoinColumn(
+					name = "course_id",
+					referencedColumnName = "id"
+			)
+	)
+	@ManyToMany(targetEntity = Course.class, fetch = FetchType.EAGER,
+	cascade = {CascadeType.DETACH, CascadeType.MERGE,
+	CascadeType.PERSIST, CascadeType.REFRESH})
+	List<Course> courses;
+	
 }
